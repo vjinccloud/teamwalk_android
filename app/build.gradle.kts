ffc4d141bcfbf5b808d4e02161dc7c123e86ea67
you55642 <dk159357852@gmail.com>
2026-02-03 00:27:58 +0800

    feat. 增加版號 3.0.17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 55
-        versionName = "3.0.16"
+        versionName = "3.0.17"
 
 //        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
