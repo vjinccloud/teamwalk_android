c833dbbd9dbd5953475213cf1510b640f2b19c1f
you55642 <dk159357852@gmail.com>
2026-03-30 19:20:11 +0800

    feat. 修改文案 版本增加3.0.34 versionCode 73

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.taiwanlife.teamwalk"
         minSdk = 29
         targetSdk = 36
-        versionCode = 72
-        versionName = "3.0.33"
+        versionCode = 73
+        versionName = "3.0.34"
 
 //        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
