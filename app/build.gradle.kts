c49cad55c3c783795d337baeaea3a85b6cfa3cf0
Wade <tony91097@gmail.com>
2026-05-09 00:21:37 +0800

    chore. 升版 v3.0.44 (vc84)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.taiwanlife.teamwalk"
         minSdk = 29
         targetSdk = 36
-        versionCode = 83
-        versionName = "3.0.43"
+        versionCode = 84
+        versionName = "3.0.44"
 
 //        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
