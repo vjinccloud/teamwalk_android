2056239a0b3187fa0cfcc098f0e5b02265c19e74
Wade <tony91097@gmail.com>
2026-06-26 10:03:06 +0800

    chore. bump 3.0.45 (vc85)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.taiwanlife.teamwalk"
         minSdk = 29
         targetSdk = 36
-        versionCode = 84
-        versionName = "3.0.44"
+        versionCode = 85
+        versionName = "3.0.45"
 
 //        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
